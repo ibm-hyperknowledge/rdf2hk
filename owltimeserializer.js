b573c784aa574ac1c46ae2d97336d08a1fbe5e78
Elton Soares <eltons@ibm.com>
2020-11-18 00:26:55 -0300

    Refactoring OWL Time conversion: (1) suppress hasBeginning/hasEnd links when applying OWLTime conversion; (2) propagate instant begin/end to associated intervals; (3) use the instant ID as begin/end in the case of indefinite instants; (4) convert interval, proper interval and date time interval simillarly.

--- owltimeserializer.js
+++ owltimeserializer.js
@@ -4,58 +4,19 @@
  */
 "use strict";
 
-const rdf               = require("./rdf");
-const xml               = require("./xmlschema");
-const owltime           = require("./owltime");
-const Utils             = require("./utils");
+const INSTANT_URI = "<http://www.w3.org/2006/time#Instant>";
+const INTERVAL_URI = "<http://www.w3.org/2006/time#Interval>";
+const PROPER_INTERVAL_URI = "<http://www.w3.org/2006/time#ProperInterval>";
+const DATE_TIME_INTERVAL = "<http://www.w3.org/2006/time#DateTimeInterval>";
+const HAS_BEGINNING_URI = "<http://www.w3.org/2006/time#hasBeginning>";
+const HAS_END_URI = "<http://www.w3.org/2006/time#hasEnd>";
+const DATE_TIME_URI = "<http://www.w3.org/2006/time#inXSDDateTime>";
 
-class OwlTimeSerializer
-{
-	constructor (sharedGraph, options)
-	{
-        this.graph = sharedGraph;
-        this.timeContext = options.timeContext;
-	}
-
-	serializeTemporalAnchorBind (entity, entities, subjectLabel, objectLabel, subjId, objId, defaultGraph, context)
-	{
-        if(this.timeContext === subjId && subjId === objId)
-        {
-            // serialize triple using timeContext anchors
-            subjId = entity.binds[subjectLabel][subjId][0];
-            objId = entity.binds[objectLabel][objId][0];
-            context = (entities.hasOwnProperty(entity.parent) ? entities[entity.parent].parent : defaultGraph) || defaultGraph;
-            this.graph.add(subjId, entity.connector, objId, context);
-        }
-        else
-        {
-            this.graph.add(subjId, entity.connector, objId, context);
-        }
-    }
-    
-    serializeTemporalAnchorProperty(interfaceNode, p, prop, parentUri)
-    {
-        if(p === rdf.TYPE_URI && Array.isArray(prop))
-        {
-            for(let i = 0; i < prop.length; i++)
-            {
-                this.graph.add(interfaceNode, p, prop[i], parentUri);
-            }
-            return;
-        }
-        else if(p === 'begin')
-        {
-            this.graph.add(interfaceNode, owltime.DATE_TIME_URI, Utils.createLiteralObject(prop, null, xml.DATETIME_URI), parentUri);
-        }
-        else if(p === 'end')
-        {
-            this.graph.add(interfaceNode, owltime.DATE_TIME_URI, Utils.createLiteralObject(prop, null, xml.DATETIME_URI), parentUri);
-        }
-        this.graph.add(interfaceNode, p, Utils.createLiteralObject(prop), parentUri);
-    }
-
-
-
-}
-
-module.exports = OwlTimeSerializer;
+exports.INSTANT_URI = INSTANT_URI;
+exports.INTERVAL_URI = INTERVAL_URI;
+exports.PROPER_INTERVAL_URI = PROPER_INTERVAL_URI;
+exports.DATE_TIME_INTERVAL = DATE_TIME_INTERVAL;
+exports.INTERVAL_URIS = [INTERVAL_URI, PROPER_INTERVAL_URI, DATE_TIME_INTERVAL];
+exports.HAS_BEGINNING_URI = HAS_BEGINNING_URI;
+exports.HAS_END_URI = HAS_END_URI;
+exports.DATE_TIME_URI = DATE_TIME_URI;
