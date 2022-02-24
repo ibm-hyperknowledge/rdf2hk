f1f715aada555850e57260cce2af42a425637d6c
Marcelo Machado <marcelo.oc.machado@gmail.com>
2022-02-24 15:11:52 -0300

    Added virtual entities constants.

--- constants.js
+++ constants.js
@@ -2,20 +2,26 @@
  * Copyright (c) 2016-present, IBM Research
  * Licensed under The MIT License [see LICENSE for details]
  */
+
 "use strict";
 
-exports.HK_PROTOCOL							= "hk";
-exports.DEFAULT_SUBJECT_ROLE 	            = "subject";
-exports.DEFAULT_OBJECT_ROLE 	            = "object";
-exports.HK_ID_PREFIX             			= `${exports.HK_PROTOCOL}://id`;
-exports.HK_ANCHOR_PREFIX		            = `${exports.HK_PROTOCOL}://a`;
-exports.HK_BLANK_NODE_PREFIX 		        = `${exports.HK_PROTOCOL}://b`;
-exports.HK_REFERENCE_PREFIX 			    = `${exports.HK_PROTOCOL}://ref`;
-exports.HK_ROLE_PREFIX 			    		= `${exports.HK_PROTOCOL}://role`;
-exports.HK_LINK_PREFIX 				        = `${exports.HK_PROTOCOL}://link`;
-exports.HK_NODE_PREFIX 				        = `${exports.HK_PROTOCOL}://node`;
+exports.HK_PROTOCOL = "hk";
+exports.DEFAULT_SUBJECT_ROLE = "subject";
+exports.DEFAULT_OBJECT_ROLE = "object";
 
-exports.HK_NULL 				            = `${exports.HK_ID_PREFIX}/null`;
-exports.MIMETYPE_APPLICATION_TURTLE 		= "application/turtle";
-exports.MIMETYPE_TEXT_TURTLE 		        = "text/turtle";
-exports.MIMETYPE_APPLICATION_NTRIPLE 		= "application/n-triples";
+exports.HK_NULL = `${exports.HK_ID_PREFIX}/null`;
+
+exports.HK_ID_PREFIX = `${exports.HK_PROTOCOL}://id`;
+exports.HK_ANCHOR_PREFIX = `${exports.HK_PROTOCOL}://a`;
+exports.HK_BLANK_NODE_PREFIX = `${exports.HK_PROTOCOL}://b`;
+exports.HK_REFERENCE_PREFIX = `${exports.HK_PROTOCOL}://ref`;
+exports.HK_ROLE_PREFIX = `${exports.HK_PROTOCOL}://role`;
+exports.HK_LINK_PREFIX = `${exports.HK_PROTOCOL}://link`;
+exports.HK_NODE_PREFIX = `${exports.HK_PROTOCOL}://node`;
+
+exports.HK_VIRTUAL_NODE_PREFIX = `${exports.HK_PROTOCOL}://virtualnode`;
+exports.HK_VIRTUAL_CONTEXT_PREFIX = `${exports.HK_PROTOCOL}://virtualcontext`;
+
+exports.MIMETYPE_APPLICATION_TURTLE = "application/turtle";
+exports.MIMETYPE_TEXT_TURTLE = "text/turtle";
+exports.MIMETYPE_APPLICATION_NTRIPLE = "application/n-triples";
