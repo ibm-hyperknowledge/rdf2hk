17e415a51e6c96b0df2b45e5ab303f0d7761af1d
Elton Soares <eltons@ibm.com>
2023-03-21 20:16:21 -0300

    adding automatic literal object unescaping to JSON graph

--- jsongraph.js
+++ jsongraph.js
@@ -49,8 +49,10 @@ JSONGraph.prototype.forEachStatement = function (callback)
 	for(let i = 0; i < this.triples.length; i++)
 	{
 		let t = this.triples[i];
-		
-		callback(t[0], t[1], t[2], t[3]);
+		let object = t[2];
+		object = object.includes(`"`) ? this.parseLiteral(object) : object;
+		t[2] = object;
+		callback(t[0], t[1], object, t[3]);
 	}
 }
 
@@ -74,6 +76,9 @@ JSONGraph.prototype.fromBGP = function (s = null, p = null, o = null, g = null)
 			}
 			else if(o === t[2])
 			{
+				let object = t[2];
+				object = object.includes(`"`) ? this.parseLiteral(object) : object;
+				t[2] = object;
 				out.push(t);
 			}
 			else if(g === t[3])
@@ -94,6 +99,68 @@ JSONGraph.prototype.fromBGP = function (s = null, p = null, o = null, g = null)
 JSONGraph.prototype.graphSize = function ()
 {
 	return this.statementCounter;	
+}
+
+JSONGraph.prototype.parseLiteral = function (literalStr) 
+{
+	// Ensure we have enough lookahead to identify triple-quoted strings
+	const quoteStr = `"`;
+	if (literalStr.length >= 3 && literalStr[0] == quoteStr && literalStr[literalStr.length - 1] == quoteStr) 
+	{
+		literalStr = literalStr.substring(1, literalStr.length - 1);
+		if (literalStr.includes(`\\"`)) return this.unescapeLiteral(literalStr);
+	}
+	return literalStr;
+}
+
+// This method was based on the _unescape function of the N3Lexer.js file from https://github.com/rdfjs/N3.js
+JSONGraph.prototype.unescapeLiteral = function (literal) 
+{
+	let invalidEscaping = false;
+	const escapeSequence = /\\u([a-fA-F0-9]{4})|\\U([a-fA-F0-9]{8})|\\([^])/g;
+	const escapeReplacements = {
+		'\\': '\\',
+		"'": "'",
+		'"': '"',
+		'n': '\n',
+		'r': '\r',
+		't': '\t',
+		'f': '\f',
+		'b': '\b',
+		'_': '_',
+		'~': '~',
+		'.': '.',
+		'-': '-',
+		'!': '!',
+		'$': '$',
+		'&': '&',
+		'(': '(',
+		')': ')',
+		'*': '*',
+		'+': '+',
+		',': ',',
+		';': ';',
+		'=': '=',
+		'/': '/',
+		'?': '?',
+		'#': '#',
+		'@': '@',
+		'%': '%'
+	};
+	const unescapedLiteral = literal.replace(escapeSequence, (sequence, unicode4, unicode8, escapedChar) => 
+	{
+		// 4-digit unicode character
+		if (typeof unicode4 === 'string') return String.fromCharCode(Number.parseInt(unicode4, 16)); // 8-digit unicode character
+		if (typeof unicode8 === 'string') 
+		{
+			let charCode = Number.parseInt(unicode8, 16);
+			return charCode <= 0xFFFF ? String.fromCharCode(Number.parseInt(unicode8, 16)) : String.fromCharCode(0xD800 + ((charCode -= 0x10000) >> 10), 0xDC00 + (charCode & 0x3FF));
+		} // fixed escape sequence
+		if (escapedChar in escapeReplacements) return escapeReplacements[escapedChar]; // invalid escape sequence
+		invalidEscaping = true;
+		return '';
+	});
+	return invalidEscaping ? literal : unescapedLiteral;
 }
 
 module.exports = JSONGraph;
